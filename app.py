b02d56a521bd16f1e23e9f52ab751693d11b148d
fitriadyaa <fitrianiwidyaf@gmail.com>
2024-01-29 09:53:24 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,111 @@
+import os
+
+import google.generativeai as genai
+from flask import Flask, Response, jsonify, request, stream_with_context
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key = os.environ.get('API_KEY'))
+
+app = Flask(__name__)
+CORS(app)
+
+# Set up the model
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 4096,
+}
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    }
+]
+
+
+@app.route("/")
+def index():
+    return jsonify({
+        "status": {
+            "code": 200,
+            "message": "Success fetching the API"
+        },
+        "data": None,
+    }), 200
+
+
+@app.route("/generate_text", methods=["GET", "POST"])
+def generate_text():
+    if request.method == "POST":
+        input_data = request.get_json()
+        prompt = input_data["prompt"]
+        model = genai.GenerativeModel(model_name="gemini-pro",
+                                      generation_config=generation_config,
+                                      safety_settings=safety_settings)
+        text_result = model.generate_content(prompt)
+        return jsonify({
+            "status": {
+                "code": 200,
+                "message": "Success generate text",
+            },
+            "data": {
+                "result": text_result.text,
+            }
+        }), 200
+    else:
+        return jsonify({
+            "status": {
+                "code": 405,
+                "message": "Method not allowed"
+            },
+            "data": None
+        }), 405
+
+
+@app.route("/generate_text_stream", methods=["GET", "POST"])
+def generate_text_stream():
+    if request.method == "POST":
+        input_data = request.get_json()
+        prompt = input_data["prompt"]
+        model = genai.GenerativeModel(model_name="gemini-pro",
+                                      generation_config=generation_config,
+                                      safety_settings=safety_settings)
+
+        def generate_stream():
+            response = model.generate_content(prompt, stream=True)
+            for chunk in response:
+                print(chunk.text)
+                yield chunk.text + "\n"
+
+        return Response(stream_with_context(generate_stream()), mimetype="text/plain")
+    else:
+        return jsonify({
+            "status": {
+                "code": 405,
+                "message": "Method not allowed"
+            },
+            "data": None
+        }), 405
+
+
+if __name__ == "__main__":
+    app.run(debug=True,
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080)))
---
+++ script.js
@@ -0,0 +1,48 @@
+const generateButton = document.getElementById("generateButton");
+const promptInput = document.getElementById("prompt");
+const generatedText = document.getElementById("generatedText");
+
+generateButton.addEventListener("click", async () => {
+  const prompt = promptInput.value;
+  if (prompt.trim() === "") {
+    alert("Please enter a prompt.");
+    return;
+  }
+
+  // Make a POST request to the Flask API
+  const response = await fetch("http://192.168.31.157:8080/generate_text_stream", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ prompt }),
+  });
+
+  if (!response.ok) {
+    console.error("Error:", response.statusText);
+    generatedText.textContent = "Error occurred while generating text.";
+    return;
+  }
+
+  console.log(response);
+
+  const reader = response.body.getReader();
+  const decoder = new TextDecoder();
+  let isFinished = false;
+  let generatedTextContent = "";
+
+  while (!isFinished) {
+    const { done, value } = await reader.read();
+
+    if (done) {
+      isFinished = true;
+      break;
+    }
+
+    const chunkText = decoder.decode(value);
+    generatedTextContent += chunkText;
+    generatedText.innerHTML = generatedTextContent;
+    await new Promise((resolve) => setTimeout(resolve, 50));
+  }
+
+});
